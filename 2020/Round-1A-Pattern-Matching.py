dd360b8360a46a7431d0cc96a5e28ef76f3c9257
osc245 <67892950+osc245@users.noreply.github.com>
2021-01-21 15:42:10 +1300

    Update Round-1A-Pattern-Matching.py

--- 2020/Round-1A-Pattern-Matching.py
+++ 2020/Round-1A-Pattern-Matching.py
@@ -4,7 +4,5 @@ for case in range(1, int(input()) + 1):
     words = [input().split("*") for _ in range(int(input()))]
     prefs, suffs = [w[0] for w in words], [w[1] for w in words]
     maxPref, maxSuff = max(prefs, key=len), max(suffs, key=len)
-    ans = "*"
-    if all([maxPref[:len(p)] == p for p in prefs]) and all(maxSuff[::-1][:len(s)] == s[::-1] for s in suffs):
-        ans = maxPref + maxSuff
+    ans = maxPref + maxSuff if all([maxPref[:len(p)] == p for p in prefs]) and all(maxSuff[::-1][:len(s)] == s[::-1] for s in suffs) else "*"
     print("Case #{}: {}".format(case, ans))
