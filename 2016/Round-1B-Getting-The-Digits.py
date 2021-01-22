6b8d1bccbd2cc9db8d5002a7597aab9f137cb826
osc245 <67892950+osc245@users.noreply.github.com>
2021-01-23 08:54:09 +1300

    Update Round-1B-Getting-The-Digits.py

--- 2016/Round-1B-Getting-The-Digits.py
+++ 2016/Round-1B-Getting-The-Digits.py
@@ -1,6 +1,8 @@
-# incomplete
+info = [("Z", "ZERO", 0), ("W", "TWO", 2), ("U", "FOUR", 4), 
+("X", "SIX", 6), ("O", "ONE", 1), ("R" , "THREE", 3), 
+("F" , "FIVE", 5), ("V" , "SEVEN", 7), ("H", "EIGHT", 8), 
+("N" , "NINE", 9)]
 
-info = [("Z", "ZERO", 0), ("W", "TWO", 2), ("U", "FOUR", 4), ("X", "SIX", 6), ("H", "EIGHT", 8)]
 for case in range(1, int(input()) + 1):
     string = input()
     nums = []
@@ -9,6 +11,5 @@ for case in range(1, int(input()) + 1):
             for c in x[1]:
                 string = string.replace(c, "", 1)
             nums.append(x[2])
-            print(string)
     ans = "".join([str(x) for x in sorted(nums)])
     print("Case #{}: {}".format(case, ans))
