bf5211a3f00b81be238ee399a2a011dcd2f1a29e
osc245 <67892950+osc245@users.noreply.github.com>
2021-01-23 09:48:01 +1300

    Update Round-1C-Senate-Evacuation.py

--- 2016/Round-1C-Senate-Evacuation.py
+++ 2016/Round-1C-Senate-Evacuation.py
@@ -4,7 +4,10 @@ for case in range(1, int(input()) + 1):
     input()
     partySize = input().split()
     people = [(chr(ord("A") + i), int(partySize[i])) for i in range(len(partySize))]
-    people.sort(key=lambda x: -x[1])
-    print(people)
     ans = ""
+    # case for when len(people) == 1 or 2
+    while # not all 0:
+        # don't sort each time just swap around the third index with first and second
+        people.sort(key=lambda x: -x[1])
+        print(people)
     print("Case #{}: {}".format(case, ans))
