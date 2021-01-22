2b6ac9d0be4d160d97f94f424cfff728eb563e62
sykesosca <sykesosca@vuw.ac.nz>
2021-01-22 16:36:39 +1300

    reformatting

---
+++ 2015/Qualification-Round-Standing-Ovation.py
@@ -0,0 +1,8 @@
+for case in range(1, int(input()) + 1):
+    maxLevel, people = input().split()
+    crowd = [int(n) for n in people]
+    count = extra = 0
+    for i in range(0, int(maxLevel) + 1):
+        extra += max(0, i - count)
+        count += max(0, i - count) + crowd[i]
+    print("Case #{}: {}".format(case, extra))
