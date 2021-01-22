a460a8ea18799ab59ac43fd6ddd4465032386bc8
osc245 <67892950+osc245@users.noreply.github.com>
2021-01-22 15:38:04 +1300

    Create 2011-Qualification-Round-Bot-Trust.py

---
+++ 2011-Qualification-Round-Bot-Trust.py
@@ -0,0 +1,14 @@
+for case in range(1, int(input()) + 1):
+    line = input().split()
+    robot = [line[i] for i in range(1, len(line)) if i % 2]
+    pos = [int(line[i]) for i in range(1, len(line)) if i % 2 == 0]
+    info = [[1, 0], [1, 0]] # [[oTime, oPos], [bTime, bPos]]
+    ans = 0
+    for i in range(len(robot)):
+        index = 1 if robot[i] == "B" else 0
+        time = max(0, abs(pos[i] - info[index][0]) - info[index][1]) + 1
+        ans += time
+        info[1 - index][1] += time
+        info[index][1] = 0
+        info[index][0] = pos[i]
+    print("Case #{}: {}".format(case, ans))
