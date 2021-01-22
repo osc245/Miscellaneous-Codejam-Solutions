b8a5f3f7a703855b7445058a5cf557b3b40b9a58
osc245 <67892950+osc245@users.noreply.github.com>
2021-01-22 15:34:01 +1300

    Create 2012-Qualification-Round-Dancing-With-The-Googlers.py

---
+++ 2012-Qualification-Round-Dancing-With-The-Googlers.py
@@ -0,0 +1,11 @@
+for case in range(1, int(input()) + 1):
+    line = [int(x) for x in input().split()]
+    ans = 0
+    suprise, cutOff = line[1:3]
+    for score in line[3:]:
+        if score >= cutOff*3 - 2*min(1, cutOff):
+            ans += 1
+        elif score >= cutOff*3 - 2*min(2, cutOff) and suprise != 0:
+            ans += 1
+            suprise -= 1
+    print("Case #{}: {}".format(case, ans))
