7096e47e4e3057095a88e61ee21911526fa7e671
osc245 <67892950+osc245@users.noreply.github.com>
2021-01-22 15:19:39 +1300

    Create 2014-Qualification-Round-Cookie-Clicker-Alpha.py

---
+++ 2014-Qualification-Round-Cookie-Clicker-Alpha.py
@@ -0,0 +1,8 @@
+for case in range(1, int(input()) + 1):
+    cost, extra, finish = [float(n) for n in input().split()]
+    curr, time = 2, 0
+    while finish/curr >= cost/curr + finish/(curr + extra):
+        time += cost/curr
+        curr += extra
+    time += finish/curr
+    print("Case #{}: {}".format(case, time))
