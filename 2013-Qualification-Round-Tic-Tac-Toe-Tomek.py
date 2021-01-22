46383ca0b2ac2fa9fb4be7ed411fab6d4d4cc771
osc245 <67892950+osc245@users.noreply.github.com>
2021-01-22 15:30:11 +1300

    Create 2013-Qualification-Round-Tic-Tac-Toe-Tomek.py

---
+++ 2013-Qualification-Round-Tic-Tac-Toe-Tomek.py
@@ -0,0 +1,15 @@
+for case in range(1, int(input()) + 1):
+    board = rows = [list(input()) for _ in range(4)]
+    rows.extend(zip(*board)) # adding columns
+    rows.append([board[i][i] for i in range(4)]) # adding diagonals
+    rows.append([board[i][-i - 1] for i in range(4)])
+    ans = "Game has not completed"
+    if all(all(s != "." for s in row) for row in board):
+            ans = "Draw"
+    for row in rows:
+        if all(s == "X" or s == "T" for s in row):
+            ans = "X won"
+        if all(s == "O" or s == "T" for s in row):
+            ans = "O won"
+    print("Case #{}: {}".format(case, ans))
+    input()
