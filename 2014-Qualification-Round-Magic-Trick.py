0476035937c50d004a8a08a066f5d24d21ef14ce
osc245 <67892950+osc245@users.noreply.github.com>
2021-01-22 15:21:21 +1300

    Create 2014-Qualification-Round-Magic-Trick.py

---
+++ 2014-Qualification-Round-Magic-Trick.py
@@ -0,0 +1,11 @@
+for case in range(1, int(input()) + 1):
+    row1 = int(input())
+    nums = set([input().split() for _ in range(4)][row1 - 1])
+    row2 = int(input())
+    nums &= set([input().split() for _ in range(4)][row2 - 1])
+    ans = "Volunteer cheated!"
+    if len(nums) == 1:
+        ans = list(nums)[0]
+    elif len(nums) > 1:
+        ans = "Bad magician!"
+    print("Case #{}: {}".format(case, ans))
