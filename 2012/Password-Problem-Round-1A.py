84dc80f230233edf241f220da7ccb454d78e7eb5
sykesosca <sykesosca@vuw.ac.nz>
2021-03-06 19:28:49 +1300

    another solution

---
+++ 2012/Password-Problem-Round-1A.py
@@ -0,0 +1,9 @@
+for case in range(1, int(input()) + 1):
+    done, total = [int(x) for x in input().split()]
+    probabilities = [float(p) for p in input().split()]
+    ans = total + 2
+    correctProb = 1
+    for i, prob in enumerate(probabilities):
+        correctProb *= prob
+        ans = min(ans, done + 2*(total - i) - correctProb*(total + 1))
+    print("Case #{}: {}".format(case, ans))
